ff0ec3c4355b3cf51f99eff7ca4e0d00ff2c492d
shriyansh kasoundhan <shriyanshkasoudhan@gmail.com>
2024-07-17 17:27:38 +0530

    first commit

---
+++ src/main/java/com/course/coursedetailsrepo/CourseDetailsRepoApplication.java
@@ -0,0 +1,13 @@
+package com.course.coursedetailsrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDetailsRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDetailsRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/coursedetailsrepo/CourseDetailsRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.coursedetailsrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseDetailsRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
